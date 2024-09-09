a2a8741d2443153c054726da69250cd5d43fdf3b
Avram Cosmin <avram.cosmin@vlah.io>
2024-09-09 23:46:44 +0300

    Updated dependencies.

--- projects/angular-tools-test/src/app/app.component.spec.ts
+++ projects/angular-tools-test/src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent]
     }).compileComponents();
   });
 
@@ -14,16 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-tools-test' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-tools-test');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-tools-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, angular-tools-test'
+    );
   });
 });
